5f9b91cc730c910955e979884bf4f888d52496d2
jNullj <15849761+jNullj@users.noreply.github.com>
2025-01-20 23:45:00 +0200

    refactor new event api into one function

--- web/index.mjs
+++ web/index.mjs
@@ -34,7 +34,7 @@ app.use('/icons', express.static(__dirname + '/icons'))
 app.use('/manifest.json', express.static(__dirname + '/manifest.json'))
 app.use('/serviceWorker.js', express.static(__dirname + '/serviceWorker.js'))
 
-app.post('/api/enter', (req, res) => {
+function handleEvent(req, res, eventType) {
     // override time with the time sent in the request (for sync), or use the current time
     const time = new Date(req?.body?.time || new Date())
     if (history.isTimeInHistory(time)) {
@@ -42,117 +42,58 @@ app.post('/api/enter', (req, res) => {
     }
 
     // if first entry, just push it to the history
+    // TODO edge cases for previous month/next month
     if (history.isEmpty) {
-        history.add(new HistoryItem('enter', time))
+        history.add(new HistoryItem(eventType, time))
         return res.sendStatus(200)
     }
 
     // events might sync from remote client, this is the logic to handle all cases
-    // event before | event after | result
-    // enter        | enter       | do nothing
-    // enter        | exit        | do nothing
-    // exit         | enter       | replace enter event
-    // exit         | exit        | add event
-    // none         | enter       | replace enter event
-    // none         | exit        | add event
-    // exit         | none        | add event
-    // enter        | none        | do nothing
-    const lastEvent = history.last
     const firstEvent = history.first
-    if (time < firstEvent.time && firstEvent.type === 'exit') {
+    const lastEvent = history.last
+    if (time < firstEvent.time && firstEvent.type !== eventType) {
         const prevMonth = new Date(history.loadedFile)
         prevMonth.setMonth(prevMonth.getMonth() - 1)
         const prevMonthDb = new History(prevMonth)
         const lastEventPrevMonth = prevMonthDb.last
-        if(lastEventPrevMonth?.type !== 'enter') { history.add(new HistoryItem('enter', time)) }
+        if (lastEventPrevMonth?.type !== eventType) { history.add(new HistoryItem(eventType, time)) }
         return res.sendStatus(200)
-    }else if (time < firstEvent.time && firstEvent.type === 'enter') {
-        history.array[0] = new HistoryItem('enter', time)
+    } else if (time < firstEvent.time && firstEvent.type === eventType) {
+        // refactor into History class
+        history.array[0] = new HistoryItem(eventType, time)
+        history.sortByTimeAsc()
+        history.saveToFile()
         return res.sendStatus(200)
-    }else if (time > lastEvent.time && lastEvent.type === 'exit') {
-        history.add(new HistoryItem('enter', time))
+    } else if (time > lastEvent.time && lastEvent.type !== eventType) {
+        history.add(new HistoryItem(eventType, time))
         return res.sendStatus(200)
-    }else if (time > lastEvent.time && lastEvent.type === 'enter') {
+    } else if (time > lastEvent.time && lastEvent.type === eventType) {
         return res.sendStatus(200)
-    }else{
+    } else {
         // time is between two events
         const nextEvent = history.firstEventAfter(time)
         const previousEvent = history.firstEventBefore(time)
-        if (previousEvent.type === 'enter') {
+        if (previousEvent.type === eventType) {
             return res.sendStatus(200)
         }
-        // prior event is an exit
-        if (nextEvent.type === 'exit') {
-            history.add(new HistoryItem('enter', time))
+        // prior event is an opposite type
+        if (nextEvent.type !== eventType) {
+            history.add(new HistoryItem(eventType, time))
             return res.sendStatus(200)
-        }else{
-            // next event is also enter, replace it as entrance time is earlier
-            // TODO REFACTOR EVERYTHING HERE INTO HISTORY CLASS
+        } else {
+            // next event is also the same type, replace it as the time is earlier
+            // refactor into History class, similar to first event case where next is same type
             nextEvent.time = time
+            history.sortByTimeAsc()
+            history.saveToFile()
             return res.sendStatus(200)
         }
     }
-})
+}
 
-app.post('/api/exit', (req, res) => {
-    // override time with the time sent in the request (for sync), or use the current time
-    const time = new Date(req?.body?.time || new Date())
-    if (history.isTimeInHistory(time)) {
-        return res.status(422).send('Time already exists in history')
-    }
+app.post('/api/enter', (req, res) => handleEvent(req, res, 'enter'))
 
-    // if first entry, just push it to the history
-    if (history.isEmpty) {
-        history.add(new HistoryItem('exit', time))
-        return res.sendStatus(200)
-    }
-
-    // events might sync from remote client, this is the logic to handle all cases
-    // event before | event after | result
-    // exit         | exit        | do nothing
-    // exit         | enter       | do nothing
-    // enter        | exit        | replace exit event
-    // enter        | enter       | add event
-    // none         | exit        | replace exit event (if prev month fits)
-    // none         | enter       | add event (if prev month fits)
-    // enter        | none        | add event
-    // exit         | none        | do nothing
-    const firstEvent = history.first
-    const lastEvent = history.last
-    if (time < firstEvent.time && firstEvent.type === 'enter') {
-        const prevMonth = new Date(history.loadedFile)
-        prevMonth.setMonth(prevMonth.getMonth() - 1)
-        const prevMonthDb = new History(prevMonth)
-        const lastEventPrevMonth = prevMonthDb.last
-        if(lastEventPrevMonth?.type !== 'exit') { history.add(new HistoryItem('exit', time)) }
-        return res.sendStatus(200)
-    }else if (time < firstEvent.time && firstEvent.type === 'exit') {
-        history.array[0] = new HistoryItem('exit', time)
-        return res.sendStatus(200)
-    }else if (time > lastEvent.time && lastEvent.type === 'enter') {
-        history.add(new HistoryItem('exit', time))
-        return res.sendStatus(200)
-    }else if (time > lastEvent.time && lastEvent.type === 'exit') {
-        return res.sendStatus(200)
-    }else{
-        // time is between two events
-        const nextEvent = history.firstEventAfter(time)
-        const previousEvent = history.firstEventBefore(time)
-        if (previousEvent.type === 'exit') {
-            return res.sendStatus(200)
-        }
-        // prior event is an enter
-        if (nextEvent.type === 'enter') {
-            history.add(new HistoryItem('exit', time))
-            return res.sendStatus(200)
-        }else{
-            // next event is also exit, replace it as exit time is earlier
-            // TODO REFACTOR EVERYTHING HERE INTO HISTORY CLASS
-            nextEvent.time = time
-            return res.sendStatus(200)
-        }
-    }
-})
+app.post('/api/exit', (req, res) => handleEvent(req, res, 'exit'))
 
 app.get('/api/sessionTime', (req, res) => {
     const sessionTime = history.dailyTime()
